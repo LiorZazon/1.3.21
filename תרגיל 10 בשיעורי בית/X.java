f2c858765a22b3390cbe1887aec3c2677cf2ad67
LiorZazon <75090709+LiorZazon@users.noreply.github.com>
2021-03-03 16:54:00 +0200

    Add files via upload

---
+++ תרגיל 10 בשיעורי בית/X.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public abstract class X {
+    public void x()
+    {
+        System.out.println("one");
+    }
+    abstract void abstractMethod();
+}
+
---
+++ תרגיל 10 בשיעורי בית/Y.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Y extends X{
+    public void Y(){
+        System.out.println("two");
+    }
+    @Override
+    void abstractMethod()
+    {
+        System.out.println("three");
+    }
+
+}
